0536f316902e527fe5d48871de5d64bde2f65cdd
Umair Hasan <ioopl.com@gmail.com>
2025-11-19 16:27:57 +0000

    Initial Commit

---
+++ PushSettings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushSettings
+//
+//  Created by Umair Hasan on 19/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushSettings/PushSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushSettingsApp.swift
+//  PushSettings
+//
+//  Created by Umair Hasan on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PushSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
